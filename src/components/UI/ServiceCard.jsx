6b6d03dd8f29bb43391def0e0c8607cc0845a565
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-18 13:51:11 +0200

    Service card implementation

--- src/components/UI/ServiceCard.jsx
+++ src/components/UI/ServiceCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function ServiceCard() {
+function ServiceCard({word1, word2, image}) {
     return (
-        <div>
-
-        </div>
+        <a href={'#process'} className={'group w-full h-auto py-12 pl-8 bg-[#131313] rounded-xl flex flex-col items-start justify-center gap-6 cursor-pointer'}>
+            <img className={'group-hover:scale-125 transition-all duration-500'} src={image} alt={'Service image'} />
+            <h2 className={'text-2xl font-bold text-white tracking-wider'}>
+                {word1} <br/> {word2}
+            </h2>
+        </a>
     );
 }
 
