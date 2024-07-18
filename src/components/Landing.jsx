074de5ff94402503b65703340a213bc9925fbf46
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-18 09:07:53 +0200

    Landing section - responsiveness

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,7 @@ import ButtonPrimary from "./UI/ButtonPrimary.jsx";
 
 function Landing() {
     return (
-        <section className={'relative w-screen h-screen px-[5%] flex flex-row justify-between items-center'}>
+        <section className={'relative w-screen h-screen px-[5%] flex flex-col lg:flex-row justify-center lg:justify-between items-center'}>
             <div
                 style={{
                     backgroundImage: `url(${backgroundImage})`,
@@ -14,18 +14,17 @@ function Landing() {
                 }}
                 className={' z-20 absolute top-0 left-0 right-0 border-0 w-full h-full'}
             ></div>
-
-            <div className={'z-30 relative w-1/2 flex flex-col justify-start items-start gap-8'}>
-                <h1 className={'text-white font-semibold text-6xl leading-tight'}>
+            <div className={'z-30 relative w-full lg:w-1/2 flex flex-col justify-start items-start gap-8'}>
+                <h1 className={'text-white font-semibold text-4xl md:text-6xl leading-tight'}>
                     Creative Digital <br/> Marketing Agency
                 </h1>
-                <p className={'w-3/4 text-white text-xl'}>
+                <p className={'w-full lg:w-3/4 text-white text-xl'}>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit amet consetetur sadipscing elitr
                 </p>
                 <ButtonPrimary />
             </div>
 
-            <div className={'relative z-10 w-1/2 h-full flex flex-col justify-center items-center'}>
+            <div className={'absolute lg:relative z-10 w-full lg:w-1/2 h-full flex flex-col justify-center items-center'}>
                 <iframe className={'w-full h-full'} src="https://lottie.host/embed/43ece56d-da47-4af2-b4d6-8bb202c54fd1/3YdSFWeTrx.json"></iframe>
             </div>
 
