9604825a2bac010db4cd7aefe59f6b226799b02e
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-18 23:04:05 +0200

    Footer section - responsiveness - complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import contactImg from "../assets/message.svg";
 
 function Footer() {
     return (
-        <footer id={'contact'} className={'relative w-screen h-auto py-32 px-[5%] grid grid-cols-2'}>
+        <footer id={'contact'} className={'relative w-screen h-auto py-32 px-[5%] grid grid-cols-1 lg:grid-cols-2'}>
             <div
                 style={{
                     backgroundImage: `url(${backgroundImage})`,
@@ -16,16 +16,16 @@ function Footer() {
                 }}
                 className={' z-20 absolute top-0 left-0 right-0 border-0 w-full h-full'}
             ></div>
-            <div className={'w-full flex flex-col justify-start items-start gap-8'}>
-                <h1 className={'text-white font-semibold text-4xl md:text-6xl leading-tight'}>
+            <div className={'w-full flex flex-col justify-start items-center lg:items-start gap-8'}>
+                <h1 className={'text-white font-semibold text-4xl md:text-6xl leading-tight text-center lg:text-start'}>
                     <span className={'text-mainGreen font-bold'}>Creative</span> Digital <br/>
                     Marketing <span className={'text-mainGreen font-bold'}>Agency</span>
                 </h1>
-                <p className={'w-full lg:w-3/4 text-white text-xl'}>
+                <p className={'w-full lg:w-3/4 text-white text-xl text-center lg:text-start'}>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum
                     dolor sit amet consetetur sadipscing elitr
                 </p>
-                <div className={'w-full flex flex-row justify-start items-center gap-8'}>
+                <div className={'w-full flex flex-col lg:flex-row justify-start items-center gap-4 lg:gap-8'}>
                     <ButtonPrimary/>
                     <p className={'text-white text-xl font-light'}>or</p>
                     <div className={'flex flex-row items-center justify-center gap-2'}>
