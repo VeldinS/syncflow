30b645531851daa523eba6b8327a5738b94557a9
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-20 16:15:04 +0200

    Contact section - form service added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,18 @@
-import React, {useState} from 'react';
-import backgroundImage from "../assets/Background-3.svg";
+import React, { useState } from 'react';
+import backgroundImage from '../assets/Background-3.svg';
 
 function Contact() {
+    async function sendDataToEmailService() {
+        await emailjs.sendForm('service_ctp6gzv', 'template_gj3aw5n', '#email-form')
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    }
 
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-
     const [checkedItems, setCheckedItems] = useState({});
 
     const handleCheckboxChange = (event) => {
@@ -16,8 +23,30 @@ function Contact() {
         });
     };
 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        // Convert checkedItems object to a string
+        const checkedItemsString = Object.keys(checkedItems)
+            .filter(key => checkedItems[key])
+            .join(', ');
+
+        // Set the hidden input value to this string
+        document.getElementById('checkedItemsHidden').value = checkedItemsString;
+
+        try {
+            await sendDataToEmailService();
+            console.log('FORM SUBMITTED');
+            setEmail('');
+            setMessage('');
+            setCheckedItems({});
+        } catch (error) {
+            console.error('Error sending data:', error);
+        }
+    };
+
     return (
-        <section id={'contact'} className={'relative w-screen h-auto py-8 sm:py-16 px-[5%] flex flex-col gap-8 lg:gap-12 justify-center items-center'}>
+        <section id="contact" className="relative w-screen h-auto py-8 sm:py-16 px-[5%] flex flex-col gap-8 lg:gap-12 justify-center items-center">
             <div
                 style={{
                     backgroundImage: `url(${backgroundImage})`,
@@ -25,15 +54,19 @@ function Contact() {
                     backgroundPosition: 'center',
                     backgroundRepeat: 'repeat'
                 }}
-                className={'z-0 absolute top-0 left-0 right-0 border-0 w-full h-full'}
+                className="z-0 absolute top-0 left-0 right-0 border-0 w-full h-full"
             ></div>
-            <h1 className={'relative z-10 text-white font-semibold text-4xl md:text-6xl leading-tight text-center'}>
-                <span className={'px-2 bg-black text-mainGreen font-bold'}>Tell</span> Us <br/>
-                What You <span className={'px-2 bg-black text-mainGreen font-bold'}>Need</span>
+            <h1 className="relative z-10 text-white font-semibold text-4xl md:text-6xl leading-tight text-center">
+                <span className="px-2 bg-black text-mainGreen font-bold">Tell</span> Us <br />
+                What You <span className="px-2 bg-black text-mainGreen font-bold">Need</span>
             </h1>
-            <form className={'relative z-10 w-full lg:w-1/2 flex flex-col justify-center items-center gap-8'}>
+            <form onSubmit={handleFormSubmit} id="email-form" name="email-form" data-name="Email Form"
+                  className="relative z-10 w-full lg:w-1/2 flex flex-col justify-center items-center gap-8">
                 <input
                     placeholder="We can reach you at..."
+                    name="email"
+                    id="email"
+                    type="text"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     className={`w-full bg-transparent border-b-2 focus:border-mainGreen outline-none text-xl font-semibold placeholder-white/50 focus:placeholder-white focus:ring-0 transition-all duration-300 ${
@@ -42,6 +75,9 @@ function Contact() {
                 />
                 <input
                     placeholder="How can we help?"
+                    name="message"
+                    id="message"
+                    type="text"
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
                     className={`w-full bg-transparent border-b-2 focus:border-mainGreen outline-none text-xl font-semibold placeholder-white/50 focus:placeholder-white focus:ring-0 transition-all duration-300 ${
@@ -52,7 +88,7 @@ function Contact() {
                     {['Website From Scratch', 'Update Existing Website', 'Marketing Solution', 'SEO Tuned', 'Social Presence', 'More Clicks', 'Quality Content'].map((option, index) => (
                         <label
                             key={index}
-                            className={`flex items-center px-2 md:px-4 py-2 border rounded-2xl cursor-pointer transition-all duration-300 hover:scale-105 shadow-sm hover:shadow-mainGreen  ${
+                            className={`flex items-center px-2 md:px-4 py-2 border rounded-2xl cursor-pointer transition-all duration-300 hover:scale-105 shadow-sm hover:shadow-mainGreen ${
                                 checkedItems[option] ? 'bg-mainGreen border-mainGreen text-defaultDark' : 'bg-transparent text-white border-white/50'
                             }`}
                         >
@@ -69,11 +105,16 @@ function Contact() {
                         </label>
                     ))}
                 </div>
+                <input
+                    type="hidden"
+                    name="checkedItems"
+                    id="checkedItemsHidden"
+                />
                 <div className="flex flex-col items-center justify-center h-auto gap-4">
-                    <p className={'text-white font-semibold text-xl leading-tight text-center'}>
-                        <span className={'text-mainGreen font-bold'}>Or </span>
-                        select nothing <br/>
-                        and lets have a <span className={'text-mainGreen font-bold'}>fresh start!</span>
+                    <p className="text-white font-semibold text-xl leading-tight text-center">
+                        <span className="text-mainGreen font-bold">Or </span>
+                        select nothing <br />
+                        and lets have a <span className="text-mainGreen font-bold">fresh start!</span>
                     </p>
                     <button
                         type="submit"
@@ -81,12 +122,10 @@ function Contact() {
                     >
                         <p className="text-defaultDark font-bold text-base leading-none">Submit</p>
                     </button>
-
                 </div>
             </form>
         </section>
-    )
-        ;
+    );
 }
 
 export default Contact;
